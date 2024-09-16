b85f49e062ee21a3289160d27c04394e9aea4023
Azure0751 <58366593+abhi.0751@users.noreply.github.com>
2024-09-16 20:39:30 +0530

    deleted

--- CosmosDBTableApiDemo/CosmosDBTableApiDemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CosmosDBTableApiDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- CosmosDBTableApiDemo/CosmosDBTableApiDemo/Models/PropertyInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CosmosDBTableApiDemo.Models
-{
-    public class PropertyInputModel
-    {
-        public string Key { get; set; }
-        public string Value { get; set; }
-        public string Type { get; set; }
-    }
-}
